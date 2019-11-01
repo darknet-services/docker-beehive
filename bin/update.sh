801063d76e84f9aac941f24e2debc14d3a7368e8
Alessio <alessio@linux.com>
2019-11-01 00:07:03 +0000

    installer

--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Some global vars
-CONFIGFILE="/opt/beehive/etc/beehive.yml"
+CONFIGFILE="/opt/beehive/etc/compose/beehive.yml"
 COMPOSEPATH="/opt/beehive/etc/compose"
 RED="[0;31m"
 GREEN="[0;32m"
@@ -9,47 +9,9 @@ WHITE="[0;0m"
 BLUE="[0;34m"
 
 
-# Check for existing beehive.yml
-function fuCONFIGCHECK () {
-  echo "### Checking for beehive configuration file ..."
-  echo -n "###### $BLUE$CONFIGFILE$WHITE "
-  if ! [ -f $CONFIGFILE ];
-    then
-      echo
-      echo "[ $RED""NOT OK""$WHITE ] - No beehive configuration found."
-      echo "Please create a link to your desired config i.e. 'ln -s /opt/beehive/etc/compose/standard.yml /opt/beehive/etc/beehive.yml'."
-      echo
-      exit 1
-    else
-      echo "[ $GREEN""OK""$WHITE ]"
-  fi
-echo
-}
-
-# Let's test the internet connection
-function fuCHECKINET () {
-SITES=$1
-  echo "### Now checking availability of ..."
-  for i in $SITES;
-    do
-      echo -n "###### $BLUE$i$WHITE "
-      curl --connect-timeout 5 -IsS $i 2>&1>/dev/null
-        if [ $? -ne 0 ];
-          then
-	    echo
-            echo "###### $BLUE""Error - Internet connection test failed.""$WHITE"" [ $RED""NOT OK""$WHITE ]"
-            echo "Exiting.""$WHITE"
-            echo
-            exit 1
-          else
-            echo "[ $GREEN"OK"$WHITE ]"
-        fi
-  done;
-echo
-}
 
 # Update
-function fuSELFUPDATE () {
+function update () {
   echo "### Now checking for newer files in repository ..."
   git fetch --all
   REMOTESTAT=$(git status | grep -c "up-to-date")
@@ -75,33 +37,15 @@ echo
 }
 
 # Stop beehive to avoid race conditions with running containers with regard to the current beehive config
-function fuSTOP_beehive () {
+function stop_beehive () {
 echo "### Need to stop beehive ..."
 echo -n "###### $BLUE Now stopping beehive.$WHITE "
 systemctl stop beehive
-if [ $? -ne 0 ];
-  then
-    echo " [ $RED""NOT OK""$WHITE ]"
-    echo "###### $BLUE""Could not stop beehive.""$WHITE"" [ $RED""NOT OK""$WHITE ]"
-    echo "Exiting.""$WHITE"
-    echo
-    exit 1
-  else
-    echo "[ $GREEN"OK"$WHITE ]"
-    echo "###### $BLUE Now cleaning up containers.$WHITE "
-    if [ "$(docker ps -aq)" != "" ];
-      then
-        docker stop $(docker ps -aq)
-        docker rm $(docker ps -aq)
-    fi
-fi
-echo
-}
 
 # Let's load docker images in parallel
-function fuPULLIMAGES {
-local beehiveCOMPOSE="/opt/beehive/etc/beehive.yml"
-for name in $(cat $beehiveCOMPOSE | grep -v '#' | grep image | cut -d'"' -f2 | uniq)
+function dockerpull {
+local composeFile="/opt/beehive/etc/compose/beehive.yml"
+for name in $(cat $composeFile | grep -v '#' | grep image | cut -d'"' -f2 | uniq)
   do
     docker pull $name &
   done
@@ -109,7 +53,7 @@ wait
 echo
 }
 
-function fuUPDATER () {
+function install () {
 local PACKAGES="apache2-utils apparmor apt-transport-https aufs-tools bash-completion build-essential ca-certificates cgroupfs-mount cockpit cockpit-docker curl debconf-utils dialog dnsutils docker.io docker-compose dstat ethtool fail2ban genisoimage git glances grc html2text htop iptables iw jq libcrack2 libltdl7 lm-sensors man mosh multitail net-tools npm ntp openssh-server openssl pass prips software-properties-common syslinux psmisc pv python-pip unattended-upgrades unzip vim wireless-tools wpasupplicant"
 echo "### Now upgrading packages ..."
 dpkg --configure -a
@@ -174,28 +118,7 @@ chmod 644 -R /data/nginx/cert
 
 echo "### Now pulling latest docker images"
 echo "######$BLUE This might take a while, please be patient!$WHITE"
-fuPULLIMAGES 2>&1>/dev/null
-
-fuREMOVEOLDIMAGES "1804"
-echo "### If you made changes to beehive.yml please ensure to add them again."
-echo "### We stored the previous version as backup in /root/."
-echo "### Done, please reboot."
-echo
-}
-
-
-################
-# Main section #
-################
-
-# Got root?
-WHOAMI=$(whoami)
-if [ "$WHOAMI" != "root" ]
-  then
-    echo "Need to run as root ..."
-    sudo ./$0
-    exit
-fi
+dockerpull 2>&1>/dev/null
 
 # Only run with command switch
 if [ "$1" != "-y" ]; then
@@ -209,8 +132,7 @@ fi
 
 fuCHECK_VERSION
 fuCONFIGCHECK
-fuCHECKINET "https://index.docker.io https://github.com"
-fuSTOP_beehive
+stop_beehive
 fuBACKUP
-fuSELFUPDATE "$0" "$@"
-fuUPDATER
+update "$0" "$@"
+install
